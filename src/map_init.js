cf6c84ed1a09b555ab34de86f0a788abc1a33a62
Apetor <apetor@kremik.localdomain>
2022-12-13 12:11:50 +0100

    added function for outputing map indicator of local business

---
+++ src/map_init.js
@@ -0,0 +1,48 @@
+// funkcia spracuej response zo stranky ako json
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+
+function initMap(adresos){
+    // adresa 
+    var adresos = "brno namesti svobody"
+    // premenna adresa sa prisposobi pre http request
+    adresos.split(" ").join("%20");
+    const Http = new XMLHttpRequest();
+    const url='https://maps.googleapis.com/maps/api/geocode/json?address=' + adresos + '&key=AIzaSyA_jFvlb3oQ-FiBNlzbc5jmDuQsB0IhKJc';
+
+    cars = [0,0];
+    getJSON(url,
+    function(err, data) {
+    if (err !== null) {
+        alert('Something went wrong: ' + err);
+    } else {
+        cars = [data.results[0].geometry.location.lat,data.results[0].geometry.location.lng];
+        var latitude =  Number(data.results[0].geometry.location.lat);
+        var length = Number(data.results[0].geometry.location.lng);
+        var options = {
+            zoom:14,
+            center:{lat:latitude,lng:length}
+        }
+        var map = new google.maps.Map(document.getElementById('map'), options)
+        var marker = new google.maps.Marker({
+            position: {lat:latitude,lng:length},
+            map: map
+        });
+    }
+    });
+
+
+} 
