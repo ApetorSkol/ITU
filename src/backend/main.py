58a7de2424c0ea07247af997361958a1d1a8e47f
Kimbyark <79720357+Kimbyark@users.noreply.github.com>
2023-01-08 18:01:18 +0100

    Update main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,10 +1,20 @@
-from flask import Flask, Request, make_response   # creating flask API
+from flask import Flask, request # creating flask API
 from flask_cors import CORS
+import json
 
 # create instance of class
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/prihlasenie', methods=['GET', 'POST'])
+def prihlasenie():
+    f = open("user.json", "r")
+    print(f)
+    users = json.loads(f.read())
+    for user in users:
+        if user["email"] == request.get_data().decode("utf-8"):
+            return user
+    return None
 
 @app.route('/ping', methods=['GET', 'POST'])
 def parse_request():
